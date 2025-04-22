962864e133a7e8609586b1f1793f4d56b2ad0ce9
luizmrd <lipesilvamiranda@hotmail.com>
2025-04-22 16:28:28 -0300

    Start

---
+++ projeto/src/main/java/com/estudos/tarefas/OrganizadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorDeTarefasApplication.class, args);
+	}
+
+}
---
+++ projeto/src/test/java/com/estudos/tarefas/OrganizadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
